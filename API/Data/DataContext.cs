4ed8b27f940fe52054a9d5c6b861155d329d4153
Christian Close <ChristianClose55@gmail.com>
2023-08-31 23:16:43 -0500

    End of Section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Entities.AppUser> Users { get; set; }
+}
